6420815c72e2cb634ddfd9c6f9eb719ab46c0599
Lentscode <antonio.lentini2004@gmail.com>
2024-08-03 14:03:12 +0200

    :sparkles: Updated server entry point

--- bin/server.dart
+++ bin/server.dart
@@ -1,22 +1,28 @@
-import 'dart:io';
+import "dart:io";
 
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
+import "package:postgres_course/config/set_up.dart";
+import "package:postgres_course/routes/get_expenses.dart";
+import "package:postgres_course/routes/routes.dart";
+import "package:shelf/shelf.dart";
+import "package:shelf/shelf_io.dart";
+import "package:shelf_router/shelf_router.dart";
 
 // Configure routes.
-final _router = Router();
+final Router _router = Router()
+  ..post("/register", register)
+  ..post("/createExpense", createExpense)
+  ..get("/getExpenses/<userId>", getExpenses);
 
 void main(List<String> args) async {
+  await setUp();
   // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+  final InternetAddress ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final Handler handler = Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+  final int port = int.parse(Platform.environment["PORT"] ?? "8080");
+  final HttpServer server = await serve(handler, ip, port);
+  print("Server listening on port ${server.port}");
 }
