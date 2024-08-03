a71e2faaff124ac5aebff0f546c62be6bc59c6be
Lentscode <antonio.lentini2004@gmail.com>
2024-08-03 14:04:19 +0200

    :rotating_light: Added types

--- lib/config/set_up.dart
+++ lib/config/set_up.dart
@@ -1,21 +1,26 @@
-import 'package:get_it/get_it.dart';
-import 'package:postgres/postgres.dart';
+import "package:get_it/get_it.dart";
+import "package:postgres/postgres.dart";
 
-import '../utils/utils.dart';
+import "../utils/utils.dart";
 
-final getIt = GetIt.instance;
+final GetIt getIt = GetIt.instance;
 
 Future<void> setUp() async {
-  final db = await Connection.open(
-    Endpoint(
-      host: "localhost",
-      database: "test_db",
-      username: "user",
-      password: "password",
-    ),
-  );
+  final Connection connection = await Connection.open(
+      Endpoint(
+        host: "localhost",
+        database: "postgres",
+        username: "antonio",
+        password: "pass",
+      ),
+      settings: ConnectionSettings(sslMode: SslMode.disable));
 
-  final eventRepo = EventRepo(db);
+  final UserRepo userRepo = UserRepo(connection);
+  final ExpenseRepo expenseRepo = ExpenseRepo(connection);
+  final Auth auth = Auth(connection);
 
-  getIt.registerSingleton<EventRepo>(eventRepo);
+  getIt.registerSingleton<UserRepo>(userRepo);
+  getIt.registerSingleton<ExpenseRepo>(expenseRepo);
+  getIt.registerSingleton<Auth>(auth);
+
 }
